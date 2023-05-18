2a5b35e7303fb401ca5a898d2dcde99d565ca43f
Gustavo <gustavodarocha241@gmail.com>
2023-05-18 08:11:16 -0300

    feat: requisitos 1 e 3 completos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Iniciando Projeto #VQV"""
-    raise NotImplementedError
+    if len(word) == 0 or word[high_index] != word[low_index]:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    if target_time is None:
+        return None
+
+    for entry, exit in permanence_period:
+        if (isinstance(entry, int) and isinstance(exit, int)):
+            if entry <= target_time and exit >= target_time:
+                counter += 1
+        else:
+            return None
+
+    return counter
